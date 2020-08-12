b6de17ee010eeb4487cab98323a2fc24e594ec68
frederikja163 <fjaplv64@gmail.com>
2020-08-12 13:54:42 +0200

    sandbox

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using HtxGraphics;
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var window = new Window();
+            window.Run();
+        }
+    }
+}
---
+++ HtxGraphics/Window.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenToolkit.Mathematics;
+using OpenToolkit.Windowing;
+using OpenToolkit.Windowing.Desktop;
+
+namespace HtxGraphics
+{
+    public class Window : IDisposable
+    {
+        private GameWindow _window;
+
+        public Window(int width = 800, int height = 600, string title = "HtxGraphicsApp")
+        {
+            var windowSettings = GameWindowSettings.Default;
+            var nativeSettings = NativeWindowSettings.Default;
+            nativeSettings.Size = new Vector2i(width, height);
+            nativeSettings.Title = title;
+            _window = new GameWindow(windowSettings, nativeSettings);
+        }
+
+        public Window(GameWindowSettings windowSettings, NativeWindowSettings nativeSettings)
+        {
+            _window = new GameWindow(windowSettings, nativeSettings);
+        }
+
+        public void Run()
+        {
+            _window.Run();
+        }
+
+        public void Dispose()
+        {
+            _window.Dispose();
+        }
+    }
+}
